9d7f97af94be983e7e377bab3dacb7005993b1b9
unknown <luthandodidiza197@gmail.com>
2023-09-18 08:11:54 +0200

    added viewimagescreen

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -4,8 +4,10 @@ import { Image, View,StyleSheet, Text } from 'react-native'
 const ViewImageScreen = () => {
   return (
     <>
-    <View>
-        <Image style={styles.image} className='w-[100%] h-[100%] ' resizeMode='contain' source={require('../assets/images/chair.jpg')} />
+    <View className='bg-black flex-1'>
+      <View className='w-[50px] h-[50px] absolute top-10 left-8 bg-amber-500'></View>
+      <View className='w-[50px] h-[50px] absolute top-10 right-8 bg-slate-500'></View>
+        <Image style={styles.image} className='w-full h-full ' resizeMode='contain' source={require('../assets/images/chair.jpg')} />
      </View>
     </>
   )
@@ -14,8 +16,8 @@ const ViewImageScreen = () => {
 
 const styles = StyleSheet.create({
     image:{
-        width:'100%',
-        height:'100%'
+        // width:'100%',
+        // height:'100%'
     }
 })
 
